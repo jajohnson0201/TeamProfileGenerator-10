438b1d1093b8509b3424e67fb44b19740828af8c
joe <jajohnson0201@gmail.com>
2023-01-11 12:57:39 -0600

    added prompt functions to get info and loop through the question options

--- index.js
+++ index.js
@@ -17,20 +17,27 @@ const questionInit = [
 
 inquirer
 .prompt(questionInit).then((initData)=>{
-    data.push(initData);
+    data.push(initData.role);
     if (initData.role = "Employee"){
-        employeePrompt(initData);
+        employeePrompt(initData.role);
     } else if (initData.role = "Intern"){
-        internPrompt(initData);
+        internPrompt(initData.role);
     } else if (initData.role = "Engineer"){
-        engineerPrompt(initData);
+        engineerPrompt(initData.role);
     } else if (initData.role = "Manager"){
-        managerPrompt(initData);
+        managerPrompt(initData.role);
     }
 })
 }
 questionInitiallizer();
 
+const nextEmployeeQuestion = [{
+    type: 'list',
+    message: 'Would you like to add another role?',
+    choices: ['yes','no'],
+    default: 'yes',
+    name: 'continueq',
+}];
 const questions = [{
     type: 'input',
     message: 'What is your Name?',
@@ -48,8 +55,19 @@ const questions = [{
 }];
 function employeePrompt(role){
     inquirer
-    .prompt(questions).then((data)=>{
-
+    .prompt(concat(questions,nextEmployeeQuestion)).then((data)=>{
+        const nextEmployee = data.continueq;
+        const name = data.name;
+        const id = data.id;
+        const email = data.email;
+        const addition = new Employee(name,id,email,role);
+        console.log("Success!","Addition added");
+        data.push(addition);
+        if(nextEmployee=== "yes"){
+            questionInitiallizer();
+        } else {
+            createHTML(data);
+        }
     })
 }
 function internPrompt(role){
@@ -58,10 +76,22 @@ function internPrompt(role){
         message: 'What School are you from?',
         name: 'school'
     },]
-    const internQuestions = concat(questions,iQuestions);
+    const internQuestions = concat(questions, iQuestions, nextEmployeeQuestion);
     inquirer
     .prompt(internQuestions).then((data)=>{
-        
+        const nextEmployee = data.continueq;
+        const name = data.name;
+        const id = data.id;
+        const email = data.email;
+        const school = data.school;
+        const addition = new Intern(name,id,email,school,role);
+        console.log("Success!","Addition added");
+        data.push(addition);
+        if(nextEmployee=== "yes"){
+            questionInitiallizer();
+        } else {
+            createHTML(data);
+        }
     })
 }
 function engineerPrompt(role){
@@ -70,10 +100,22 @@ function engineerPrompt(role){
         message: 'What is your GitHub?',
         name: 'github'
     },]
-    const engineerQuestions = concat(questions, iQuestions);
+    const engineerQuestions = concat(questions, iQuestions, nextEmployeeQuestion);
     inquirer
     .prompt(engineerQuestions).then((data)=>{
-        
+        const nextEmployee = data.continueq;
+        const name = data.name;
+        const id = data.id;
+        const email = data.email;
+        const github = data.github;
+        const addition = new Engineer(name,id,email,github,role);
+        console.log("Success!","Addition added");
+        data.push(addition);
+        if(nextEmployee=== "yes"){
+            questionInitiallizer();
+        } else {
+            createHTML(data);
+        }
     })
 }
 function managerPrompt(role){
@@ -82,9 +124,21 @@ function managerPrompt(role){
         message: 'What is your Office Number?',
         name: 'officeNumber'
     },]
-    const managerQuestions = concat(questions, iQuestions);
+    const managerQuestions = concat(questions, iQuestions, nextEmployeeQuestion);
     inquirer
-    .prompt().then((data)=>{
-        
+    .prompt(managerQuestions).then((data)=>{
+        const nextEmployee = data.continueq;
+        const name = data.name;
+        const id = data.id;
+        const email = data.email;
+        const officeNumber = data.officeNumber;
+        const addition = new Manager(name,id,email,officeNumber,role);
+        console.log("Success!","Addition added");
+        data.push(addition);
+        if(nextEmployee=== "yes"){
+            questionInitiallizer();
+        } else {
+            createHTML(data);
+        }
     })
 }
